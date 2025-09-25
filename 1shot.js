68a8f516fe72b07fe0a7d10c67745e754f562da7
pafu2 <tawainashi@gmail.com>
2025-09-26 03:49:31 +0900

    Update 1shot.js

--- 1shot.js
+++ 1shot.js
@@ -1221,7 +1221,6 @@
     resetCurrentEquip.style.borderTop = 'solid 1px #000';
     resetCurrentEquip.style.cursor = 'pointer';
     resetCurrentEquip.style.color = '#a62';
-    resetCurrentEquip.style.height = '80px';
     resetCurrentEquip.style.whiteSpace = 'nowrap';
     resetCurrentEquip.style.overflow = 'hidden';
     resetCurrentEquip.addEventListener('click', ()=>{
@@ -3047,6 +3046,7 @@
     });
   })();
 })();
+
 
 
 
