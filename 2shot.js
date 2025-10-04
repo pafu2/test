84ab4d13a2bc956d8e26c77a0b3964fd24106db5
pafu2 <tawainashi@gmail.com>
2025-10-04 12:30:10 +0900

    Update 2shot.js

--- 2shot.js
+++ 2shot.js
@@ -2699,8 +2699,7 @@
               processType = 'return';
             } else if (messageType === 'retry') {
               sleepTime = 10.1;
-              loop += 1;
-              message = '(' + loop + '発目) '+ lastLine;
+              message = lastLine;
               processType = 'continue';
             } else if (messageType === 'equipError'){
                 if (loop < 9){
@@ -3088,4 +3087,5 @@
     });
   })();
 })();
+
 
