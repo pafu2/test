3a73c6a86511b0df521fac5597aafe2226c50d16
pafu2 <tawainashi@gmail.com>
2025-11-21 12:56:37 +0900

    Update 4shot_12-22-32-42.js

--- 4shot_12-22-32-42.js
+++ 4shot_12-22-32-42.js
@@ -2581,10 +2581,10 @@
     }
 
     const messageTypes = {
-      capitalRetry: [
+      capitalAttack: [
         '再建が必要です。'
       ],
-      reinforceRetry: [
+      reinforceAttack: [
         '防御設備を破壊しました。'
       ],
       retry: [
@@ -3152,5 +3152,6 @@
     });
   })();
 })();
+
 
 
