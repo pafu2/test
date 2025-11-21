726d86f7b02bc29f72acdefe25b348f5e0300538
pafu2 <tawainashi@gmail.com>
2025-11-21 21:51:07 +0900

    Update 2shot.js

--- 2shot.js
+++ 2shot.js
@@ -2004,11 +2004,7 @@
 
     const cells = grid.querySelectorAll('.cell');
     cells.forEach(async(elm) => {
-      const hasInfo = elm.dataset.rank !== undefined;
-      const isRefreshed = refreshedCells.includes(elm);
-      if(refreshAll || !hasInfo || isRefreshed) {
-        fetchSingleArenaInfo(elm)
-      }
+      fetchSingleArenaInfo(elm);
     })
   }
   async function fetchSingleArenaInfo(elm) {
@@ -3136,6 +3132,7 @@
     });
   })();
 })();
+
 
 
 
