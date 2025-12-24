1e107faab064921ba451cabe66105627842a712c
pafu2 <tawainashi@gmail.com>
2025-12-24 12:15:03 +0900

    Update 2shot.js

--- 2shot.js
+++ 2shot.js
@@ -5,6 +5,7 @@
 // @author       ぱふぱふ
 // @match        https://donguri.5ch.net/teambattle?m=hc
 // @match        https://donguri.5ch.net/teambattle?m=l
+// @match        https://donguri.5ch.net/teambattle?m=rb
 // @match        https://donguri.5ch.net/bag
 // ==/UserScript==
 
@@ -3163,6 +3164,7 @@ async function fetchSingleArenaInfo(elm) {
     });
   })();
 })();
+
 
 
 
