e5ba6a6c028d47b5b89658550de1c17d71398fa9
pafu2 <tawainashi@gmail.com>
2026-01-22 12:03:44 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -2691,8 +2691,8 @@
             if (text.startsWith('アリーナチャレンジ開始')||text.startsWith('リーダーになった')) {
               if (loop < 1){
                 loop += 1;
-                sleepTime = 1800000;
-                message = '[30分後] ' + lastLine;
+                sleepTime = 30000;
+                message = '[30秒後] ' + lastLine;
                 processType = 'continue';
               } else {
                 success = true;
