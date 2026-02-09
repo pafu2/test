4c80ada52648c33a24b114c333065e7d1f2dabe1
pafu2 <tawainashi@gmail.com>
2026-02-10 08:13:09 +0900

    Add files via upload

--- umeConsole.js
+++ umeConsole.js
@@ -2524,7 +2524,7 @@
       const now = new Date(new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
       const hour = now.getHours();
       const minute = now.getMinutes();
-      return hour >= 4 && hour < 8;
+      return hour >= 4 && hour < 12;
       // 以下指定例（変える時は上の return hour >= 4 && hour < 8; を書き換え、24:00をまたぐ指定のときは&&じゃなく||）
       // 3:00～7:59
       // return hour >= 3 && hour < 8;
