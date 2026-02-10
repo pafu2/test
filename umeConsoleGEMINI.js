b25839b1588069fcbc577b7530cd0464661703a6
pafu2 <tawainashi@gmail.com>
2026-02-11 00:40:49 +0900

    Update umeConsoleGEMINI.js

--- umeConsoleGEMINI.js
+++ umeConsoleGEMINI.js
@@ -2524,7 +2524,7 @@
       const now = new Date(new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
       const hour = now.getHours();
       const minute = now.getMinutes();
-      return hour >= 4 && hour < 24;
+      return hour >= 22 || hour < 10;
       // 以下指定例（変える時は上の return hour >= 4 && hour < 8; を書き換え、24:00をまたぐ指定のときは&&じゃなく||）
       // 3:00～7:59
       // return hour >= 3 && hour < 8;
@@ -3328,3 +3328,4 @@ console.log('mapEdge:', regions.mapEdge.length);
     });
   })();
 })();
+
