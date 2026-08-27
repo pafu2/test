02f626983e6fbc292e598f176846f143a62aaa6f
pafu2 <tawainashi@gmail.com>
2026-08-27 14:52:37 +0900

    Add files via upload

--- umeR.js
+++ umeR.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         donguri arena assist tool
-// @version      1.2.2d改 ume予約 06/11版
+// @version      1.2.2d改 ume予約 08/27版
 // @description  fix arena ui and add functions
 // @author       ぱふぱふ
 // @match        https://donguri.5ch.io/teambattle?m=hc
@@ -2684,7 +2684,7 @@
                 i++;
               }
             } else if(cellType === 'participation') {
-              if (text.startsWith('アリーナチャレンジ開始')||text.startsWith('リーダーになった')) {
+              if (text.includes('アリーナチャレンジ開始')||text.includes('リーダーになった')) {
                 loop += 1;
                 shot = true;
                 sleepTime = 1.5;
@@ -2696,7 +2696,7 @@
                 processType = 'reload';
               }
               i++;
-            } else if (text.startsWith('アリーナチャレンジ開始')||text.startsWith('リーダーになった')) {
+            } else if (text.includes('アリーナチャレンジ開始')||text.includes('リーダーになった')) {
               if (loop < 255){
                 loop += 1;
                 shot = true;
